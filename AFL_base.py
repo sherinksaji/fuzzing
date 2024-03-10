325dff08d74497fa31dae1a42d777ae42f032a54
Sherin Saji <sherinkaruvallilsaji@gmail.com>
2024-03-10 13:10:15 +0800

    isinteresting added to AFL_base.py

--- AFL_base.py
+++ AFL_base.py
@@ -1,35 +1,125 @@
-SeedQ = []
-FailureQ = []
-revealsCrashOrBug = 1
+from copy import deepcopy
 
 
-def ChooseNext(SeedQ):
-    #insert function
-    return True
+class AFL_Fuzzer:
+    def __init__(self):
+        self.seedQ = []
+        self.failureQ = []
+        self.branchBucketDict = {}
 
-def AssignEnergy(t):
-    #insert function
-    return True
+    def ChooseNext(self):
+        # insert function
+        # use self.seedQ
+        return True
 
-def MutateInput(t):
-    #insert function
-    return True
+    def AssignEnergy(self, t):
+        # insert function
 
-def isInteresting(t_prime):
-    #insert function
-    return True
+        return True
+
+    def MutateInput(self, t):
+        # insert function
+        return True
+
+    def getBranches(self, t_prime_path):
+        branches = []
+        for i in range(len(t_prime_path) - 1):
+            branches.append((t_prime_path[i], t_prime_path[i + 1]))
+        return branches
+
+    def getBranchCountDict(self, branches):
+        branchCountDict = {}
+        for branch in branches:
+            if branch not in branchCountDict.keys():
+                branchCountDict[branch] = 1
+            else:
+                branchCountDict[branch] += 1
+        return branchCountDict
+
+    def newThresholdReachedUpdate(self, branchCountDict):
+        # bucket_threshold = [1,2,3,4-7,8-15,16-31,32-127,128+]
+        prevBucketDict = deepcopy(self.branchBucketDict)
+        for k, v in branchCountDict.items():
+            if v == 1:
+                self.branchBucketDict[k] = "1"
+            elif v == 2:
+                self.branchBucketDict[k] = "2"
+            elif v == 3:
+                self.branchBucketDict[k] = "3"
+            elif v >= 4 and v <= 7:
+                self.branchBucketDict[k] = "4-7"
+            elif v >= 8 and v <= 15:
+                self.branchBucketDict[k] = "8-15"
+            elif v >= 16 and v <= 31:
+                self.branchBucketDict[k] = "16-31"
+            elif v >= 32 and v <= 127:
+                self.branchBucketDict[k] = "32-127"
+            elif v >= 128:
+                self.branchBucketDict[k] = "128+"
+
+        return prevBucketDict != self.branchBucketDict
+
+    def isInteresting(self, t_prime_path):
+        # insert function
+        # -store and update pathBranchCountDictionary key:value→ path:count
+        # -store and update pathBranchBucketDictionary key:value→path:bucket
+        # -if a bucket update was made→return true else return false
+        branches = self.getBranches(t_prime_path)
+        branchCountDict = self.getBranchCountDict(branches)
+        return self.newThresholdReached(branchCountDict)
+
+    def runTestRevealsCrashOrBug(self, t_prime):
+        # insert
+        crashOrBug = True
+        t_prime_path = ["A", "B", "C", "B", "C", "A"]
+        return crashOrBug, t_prime_path
+
+    def fuzz(self):
+        while self.seedQ[0] != None:  # and timeout?
+            t = self.ChooseNext()
+            E = self.AssignEnergy(t)
+            for i in range(1, E):
+                t_prime = self.MutateInput(t)
+                crashOrBug, t_prime_path = self.runTestRevealsCrashOrBug(t_prime)
+                if crashOrBug:
+                    self.FailureQ.append(t_prime)
+                elif self.isInteresting(t_prime_path) == True:
+                    self.SeedQ.append[(t_prime, t_prime_path)]
 
 
-def Main():
-    while SeedQ[0] != None: # and timeout? 
-        t = ChooseNext(SeedQ)
-        E = AssignEnergy(t)
-        for i in range (1, E):
-            t_prime = MutateInput(t)
-            if t_prime == 1:
-                FailureQ.append(t_prime)
-            elif isInteresting(t_prime) == True:
-                SeedQ.append(t_prime)
-        
+# def Main():
+#     while SeedQ[0] != None: # and timeout?
+#         t = ChooseNext(SeedQ)
+#         E = AssignEnergy(t)
+#         for i in range (1, E):
+#             t_prime = MutateInput(t)
+#             if t_prime == 1:
+#                 FailureQ.append(t_prime)
+#             elif isInteresting(t_prime) == True:
+#                 SeedQ.append(t_prime)
 
 
+def printDict(dictName, dict):
+    print("printing " + dictName + "...")
+    for k, v in dict.items():
+        print("key = {} : value = {}".format(k, v))
+
+
+# tests for isinterersting
+# aflFuzzer = AFL_Fuzzer()
+# path = ["A", "B", "C", "B", "C", "A"]
+# for i in range(10):
+#     branches = aflFuzzer.getBranches(path)
+#     # print("branches : ", branches)
+#     branchCountDict = aflFuzzer.getBranchCountDict(branches)
+#     printDict("branchCountDict", branchCountDict)
+#     print("updateOccurred : ", aflFuzzer.newThresholdReachedUpdate(branchCountDict))
+#     printDict("branchBucketDict", aflFuzzer.branchBucketDict)
+#     path += path[1:]
+#     print(
+#         """
+
+#             ----One test over ---
+
+#            """
+#     )
--- exploreBooFuzz.py
+++ exploreBooFuzz.py
@@ -1,5 +1,6 @@
 from boofuzz import *
 import random
+import json
 
 
 # class CustomMutation(Mutation):
@@ -66,6 +67,49 @@ import random
 #         pass
 
 
+from boofuzz import *
+import random
+
+
+# def generate_random_json():
+#     random_name = "".join(
+#         random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=10)
+#     )
+#     random_info = "abcd"
+#     random_price = str(random.randint(1, 100))
+#     return {"name": random_name, "info": random_info, "price": random_price}
+
+
+# # Define the target
+# target = Target(connection=SocketConnection("127.0.0.1", 8000, proto="tcp"))
+
+# # Define the session
+# session = Session(target=target)
+
+# s_initialize(name="Fuzz POST Request")
+# with s_block("Request-Line"):
+#     s_string("POST", fuzzable=False)
+#     s_delim(" ", fuzzable=False)
+#     s_string("/datatb/product/add/", fuzzable=False)
+#     s_delim(" ", fuzzable=False)
+#     s_string("HTTP/1.1", fuzzable=False)
+#     s_static("\r\n", name="Request-Line-CRLF")
+#     s_string("Host:", fuzzable=False)
+#     s_delim(" ", fuzzable=False)
+#     s_string("127.0.0.1:8000", fuzzable=False)
+#     s_static("\r\n", name="Host-CRLF")
+#     s_string("Content-Type:", fuzzable=False)
+#     s_delim(" ", fuzzable=False)
+#     s_string("application/json", fuzzable=False)
+#     s_static("\r\n", name="Content-Type-CRLF")
+#     s_static("\r\n", name="Header-CRLF")
+
+#     json_data = generate_random_json()
+#     json_string = json.dumps(json_data)
+#     s_static(json_string, name="json-payload")
+# session.connect(s_get("Fuzz POST Request"))
+# for i in range(1000):
+#     session.fuzz()
 # Define the target
 target = Target(connection=SocketConnection("127.0.0.1", 8000, proto="tcp"))
 
@@ -99,9 +143,7 @@ with s_block("Request-Line"):
     random_name = "".join(
         random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=10)
     )
-    random_info = "".join(
-        random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", k=10)
-    )
+    random_info = "abcd"
     random_price = str(round(random.randint(1, 100), 2))
 
     # JSON payload starts here
@@ -109,9 +151,10 @@ with s_block("Request-Line"):
     s_static('"name": "', name="json-name-label")
     s_string(random_name, name="json-name-value")
     s_static('", "info": "', name="json-info-label")
-    s_string(random_info, name="json-info-value")
+    s_static(random_info, name="json-info-value")
     s_static('", "price": ', name="json-price-label")
     s_static(random_price, name="json-price-value")
+    s_static("}", name="json-end")
 
 session.connect(s_get("Fuzz POST Request"))
 session.fuzz()
