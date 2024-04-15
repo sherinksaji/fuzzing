a02c58513d956ceb11c836970adc2d9a7a738963
NithyaRV <127846590+NithyaRV@users.noreply.github.com>
2024-04-15 14:32:12 +0800

    Create CoAPInput.py

---
+++ CoAPInput.py
@@ -0,0 +1,15 @@
+from targetInput import TargetInput
+
+
+class CoAPInput(TargetInput):
+
+    def __init__(self, payload, header, resourcepath,type, method):
+        self.payload = payload
+        self.header = header
+        self.resourcepath = resourcepath
+        self.msgtype=type
+        self.method=method
+
+    def print_CoAPInput(self):
+        input_obj=[self.payload, self.header, self.resourcepath,self.msgtype, self.method]
+        print(input_obj)
