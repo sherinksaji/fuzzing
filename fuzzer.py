e1eae773ac5ac450251a0b2aa2bb8f700dc85660
Yu Qin <yuqin.110601@gmail.com>
2024-04-18 16:40:30 +0800

    update coverage for Django, general afl fuzzer, mutator

--- fuzzer.py
+++ fuzzer.py
@@ -5,22 +5,33 @@ from mutator import Mutator
 
 
 class Fuzzer:
-    def __init__(self, len_limit,):
-      self.len_limit = len_limit
-      self.seedQ = []
-      self.failureQ = []
-      self.mutator = Mutator()
+    def __init__(self, initial_input):
+        self.seedQ = [initial_input]
+        self.failureQ = []
+        self.mutator = Mutator()
+        self.path_coverage = []
+
+    def is_interesting(self, path, input):
+        if path not in self.path_coverage:
+            self.path_coverage.append(path)
+            self.seedQ.append(input)
+            return True
+
+        return False
+
+    def chooseNext(self):
+        nextSeed = self.seedQ[0]
+        self.seedQ = self.seedQ[1:]
+        return nextSeed
+
+    def assignEnergy(self):
+        return 500
+
+    def mutate(self, attri):
+        return self.mutator.mutate(attri)
 
 
-    def generate_random_str(self):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        result_str = ''.join(random.choice(characters) for _ in range(self.len_limit))
-        self.seedQ.append(result_str)
-                
-        return result_str
-    
-    def mutate_str(self, input):
-        return self.mutator.mutate_str(input_str=input)
+
 
 
     def execute_fuzzing(self):
